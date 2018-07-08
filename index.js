6520fbd2ee9ba50a3ac299aad690ab5fa83d76ff
Joseph Parker <jparker010190@gmail.com>
2018-07-08 23:04:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function BoardMember(name, homeState, training, veto){
   this.veto = function(){
     var result = 'No, I must disagree'
     return result}
-  this.doChartiy = function(){console.log(`You can do that!`)};
+  this.approve = function(){let result = "You can do that!" return result}
+  this.doChartiy = function(){let result = `You can do that!` return result};
   this.releasePressStatement = function(){console.log("You will see great things from Scuber.")};
   this.sayHi = function(){console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)};
 }
